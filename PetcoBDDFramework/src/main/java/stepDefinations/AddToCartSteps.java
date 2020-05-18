610e9c9d7c0f2d85924b3ef8f84d6d46a730e09e
Lucky <prasanna.kandregula.lpk@gmail.com>
2020-05-18 21:34:23 +0530

    AddToCart feature and stepDefinations added

---
+++ PetcoBDDFramework/src/main/java/stepDefinations/AddToCartSteps.java
@@ -0,0 +1,66 @@
+package stepDefinations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+ 
+
+public class AddToCartSteps {
+	
+
+		@Given("^guest user is on home page$")
+	public void guest_user_is_on_home_page() {
+		
+	}
+
+	@When("^title of home page is Petco Supplies$")
+	public void title_of_home_page_is_Petco_Supplies() {
+	    
+	    
+	}
+
+	@Then("^user searches for an item$")
+	public void user_searches_for_an_item() {
+	    
+	    
+	}
+
+	@Then("^user naviagtes to PDP by choosing specific item$")
+	public void user_naviagtes_to_PDP_by_choosing_specific_item() {
+	    
+	    
+	}
+
+	@Then("^user adds item to cart$")
+	public void user_adds_item_to_cart() {
+	    
+	    
+	}
+	@Given("^registered user is on home page$")
+	public void registered_user_is_on_home_page() {
+	    
+	    
+	}
+
+	@Then("^user enters \"([^\"]*)\" and \"([^\"]*)\" in Sign In page$")
+	public void user_enters_and_in_Sign_In_page(String username, String paswword) {
+	    
+		
+	}
+
+	@Then("^user clicks on Sign In button and navigates to Home page$")
+	public void user_clicks_on_Sign_In_button_and_navigates_to_Home_page() {
+	    
+	    
+	}
+
+
+}
+ 
+ 
